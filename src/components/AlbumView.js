c938e63c8ce582f5283bbc27ae2db7893f124463
jhersbo <jhersbo@gmail.com>
2022-01-21 17:56:19 -0600

    waiting to implement useHistory

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -1,13 +1,47 @@
-// These components will be making separate API calls from the app
-// component to serve specific data about a given album
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+// import { useHistory } from 'react-router-dom'
 
 const AlbumView = () => {
-    const [ albumData, setalbumData ] = useState([])
+    // const history = useHistory()
+    const { id } = useParams()
+    const [ albumData, setAlbumData ] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const API_URL = `http://localhost:4000/song/${id}`
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setAlbumData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    // const navButtons = ()=>{
+    //     return(
+    //         <div>
+    //             <button onClick={()=>history.goBack()}>Back</button>
+    //             |
+    //             <button onClick={()=>history.push('/')}>Home</button>
+    //         </div>
+    //     )
+    // }
+
+    const justSongs = albumData.filter(entry => entry.kind === 'song')
+
+    const renderSongs = justSongs.map((song, i) => {
+        return (
+            <div key={i}>
+                <p>{song.trackName}</p>
+            </div>
+        )
+    })
 
     return (
         <div>
-            <p>Album Data Goes Here!</p>
+            {albumData[0].albumName}
+            {/* {navButtons()} */}
+            {renderSongs}
         </div>
     )
 }
--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,16 +1,48 @@
-// These components will be making separate API calls from the app
-// component to serve specific data about our artist
+// import { useHistory } from 'react-router-dom'
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 
 const ArtistView = () => {
+    // const history = useHistory()
     const {id} = useParams()
     const [ artistData, setArtistData ] = useState([])
 
+    useEffect(()=>{
+        const API_URL = `http://localhost:4000/album/${id}`
+        const fetchData = async ()=>{
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setArtistData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    // const navButtons = ()=>{
+    //     return(
+    //         <div>
+    //             <button onClick={()=>history.goBack()}>Back</button>
+    //             |
+    //             <button onClick={()=>history.push('/')}>Home</button>
+    //         </div>
+    //     )
+    // }
+
+    const justAlbums = artistData.filter(entry => entry.collectionType === 'Album')
+    const renderAlbums = justAlbums.map((album, i)=>{
+        return(
+            <div key={i}>
+                <Link to={`/album/${album.collectionId}`}>
+                    <p>{album.collectionName}</p>
+                </Link>
+            </div>
+        )
+    })
+
     return (
         <div>
-            <h2>The id that was passed was: {id}</h2>
-            <p>Artist Data Goes Here!</p>
+            {artistData[0].artistName}
+            {/* {navButtons()} */}
+            {renderAlbums}
         </div>
     )
 }
